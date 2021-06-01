619220d70f99dd5a02b092e82985373201e91557
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2021-06-01 16:23:58 -0300

    adicionando ícones

--- admin/records.php
+++ admin/records.php
@@ -10,6 +10,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/5e5fac4a54.js" crossorigin="anonymous"></script>
+
     <!-- Personal CSS -->
     <link rel="stylesheet" href="../assets/css/style.css">
     <style>
@@ -44,6 +47,14 @@
         }
         h6.lead {
             font-size: 1.1rem;
+        }
+
+        td > i {
+            margin-right: 6px;
+            cursor: pointer;
+        }
+        td > i:last-child {
+            margin: 0;
         }
         @media (max-width: 1300px) {
             p.lead {
@@ -158,7 +169,7 @@
                                                     <th scope="col">Hóspede</th>
                                                     <th scope="col">Suíte</th>
                                                     <th scope="col">Entrada</th>
-                                                    <th scope="col">Saída</th>
+                                                    <th scope="col">Saída Prevista</th>
                                                     <th scope="col">Ação</th>
                                                 </tr>
                                             </thead>
@@ -171,7 +182,14 @@
                                                         <td><?= (rand(2, 9) * time()) ?></td>
                                                         <td>01/06/21</td>
                                                         <td>05/06/21</td>
-                                                        <td>[V] [E] [D]</td>
+                                                        <td>
+                                                            <i class="fas fa-eye"
+                                                                onclick="action('view')"></i>
+                                                            <i class="fas fa-edit text-info"
+                                                                onclick="action('edit')"></i>
+                                                            <i class="fas fa-trash-alt text-danger"
+                                                                onclick="remove()"></i>
+                                                        </td>
                                                     </tr>
                                                 <?php endfor ?>
                                             </tbody>
@@ -225,5 +243,21 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
+    <script>
+        function action(action)
+        {
+            if (action === 'view') {
+                location.href = 'personal_record.php?act=view'
+            }
+            if (action === 'edit') {
+                location.href = 'personal_record.php'
+            }
+        }
+
+        function remove()
+        {
+            confirm('Apagar registro?')
+        }
+    </script>
 </body>
 </html>
